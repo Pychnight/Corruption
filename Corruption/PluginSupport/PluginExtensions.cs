849934fdee3c844d2c334107a8fe5cccbf841ecd
Timothy Barela <tbarela@timbarela.com>
2018-04-05 03:10:54 -0700

    Added TerrariaPluginExtensions.
    For helper method, LogPrint().

---
+++ Corruption/PluginSupport/PluginExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TerrariaApi.Server;
+
+namespace Corruption.PluginSupport
+{
+	public static class TerrariaPluginExtensions
+	{
+		public static void LogPrint(this TerrariaPlugin plugin, string message, TraceLevel kind = TraceLevel.Info )
+		{
+			ServerApi.LogWriter.PluginWriteLine(plugin, message, kind);
+		}
+	}
+}
