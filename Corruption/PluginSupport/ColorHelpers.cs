7beea684b08b852830d5eac0708368f257f58343
Timothy Barela <tbarela@timbarela.com>
2018-07-13 07:20:00 -0700

    Add ColorHelpers class.

---
+++ Corruption/PluginSupport/ColorHelpers.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+
+namespace Corruption.PluginSupport
+{
+	public static class ColorHelpers
+	{
+		public static string ToHexString(this Color color)
+		{
+			return color.PackedValue.ToString("x8");
+		}
+
+		public static Color FromHexString(string value)
+		{
+			try
+			{
+				var packed = Convert.ToUInt32(value, 16);
+				var result = new Color(packed);
+
+				return result;
+			}
+			catch
+			{
+				return Color.White;
+			}
+		}
+	}
+}
